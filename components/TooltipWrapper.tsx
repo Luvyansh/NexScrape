fb322027ccf7d8475b04c640f69b7069356e028d
Luvyansh <luvyansh2001@gmail.com>
2025-09-12 23:19:12 +0530

    feat(workflows): add workflow page, create dialog, Prisma schema, and TanStack Query components

---
+++ components/TooltipWrapper.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
+import { ReactNode } from "react";
+
+interface Props {
+    children: React.ReactNode;
+    content: React.ReactNode;
+    side?: "top" | "bottom" | "left" | "right";
+}
+function TooltipWrapper(props: Props) {
+    return (
+        <TooltipProvider delayDuration={0}>
+            <Tooltip>
+                <TooltipTrigger asChild>{props.children}</TooltipTrigger>
+                <TooltipContent side={props.side}>
+                    {props.content}
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    );
+}
+
+export default TooltipWrapper;
